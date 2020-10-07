a8940d32b6d580f83e6173e11c234ab882958d21
Ilimita Zensay <ilimitaweb@gmail.com>
2020-10-07 10:33:27 -0500

    Updates

---
+++ precache-manifest.cdf233093a415a972e76e1fc42f15b51.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f644f8e3ca5d1870a21c428cc526bc7b",
+    "url": "/frontendCli/index.html"
+  },
+  {
+    "revision": "ceb287593f36c98085ef",
+    "url": "/frontendCli/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "a5667a624555ed625e07",
+    "url": "/frontendCli/static/css/main.24425e0b.chunk.css"
+  },
+  {
+    "revision": "ceb287593f36c98085ef",
+    "url": "/frontendCli/static/js/2.182d5644.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/frontendCli/static/js/2.182d5644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5667a624555ed625e07",
+    "url": "/frontendCli/static/js/main.9a2041f9.chunk.js"
+  },
+  {
+    "revision": "2b1344d2f90f46bf3a36",
+    "url": "/frontendCli/static/js/runtime-main.603b8836.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/frontendCli/static/media/logo.5d5d9eef.svg"
+  }
+]);
